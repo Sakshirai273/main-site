98e9175f08d7fde3d7398dfe80a1be70c699e345
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-09-18 09:05:49 +0530

    again

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,8 +1,67 @@
-import React from "react";
+import React, { Component } from 'react';
 import { Parallax } from "react-parallax";
 import Imag from "./img/bg-3.jpg"
-
-const ContactForm = () => {
+import axios from 'axios';
+class ContactForm extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+        data: [],
+      };
+    }
+    handleInputChange = e => {
+      this.setState({
+        [e.target.name]: e.target.value,
+      });
+    };
+    
+    handleSubmit = e => {
+    //   e.preventDefault();
+      const { name, email, subject,message} = this.state;
+      const book = {
+        name,
+        email,
+        subject,
+        message
+      };
+      axios
+        .post('http://localhost:8001/create', book)
+        .then(() => console.log('Book Created'),
+  
+  
+          this.callAPI(),
+  
+        //   this.setState({
+        //     bookID: '',
+        // bookTitle: '',
+        // bookAuthor: '',
+        // data: [],
+        //   })
+     
+        )
+        
+        .catch (err => {
+        console.error(err);
+      });
+    };
+    callAPI() {
+      //API request
+      axios.get("http://localhost:3001/home").then(response => {
+  
+        //getting and setting api data into variable
+  
+        this.setState({ data: response.data });
+  
+      })
+    }
+  
+    render() {
+      
+    
     return (
         <div id="section4">
             <Parallax bgImage={Imag} strength={600}>
@@ -14,10 +73,7 @@ const ContactForm = () => {
                                     <h1>contact form</h1>
                                     <div class="row">
                                         <div class="col-sm-12">
-                                            <p>Nunc diam leo, fringilla vulputate elit lobortis, consectetur vestibulum quam.
-                                                Sed id <br />
-                                                felis ligula. In euismod libero at magna dapibus, in rutrum velit lacinia. <br />
-                                                Etiam a mi quis arcu varius condimentum.</p>
+                                            <p>Please fill this contact form our representatives will soon reach to you.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -25,20 +81,22 @@ const ContactForm = () => {
                         </div>
                         <div class="row">
                             <div class="col-lg-12">
-                                <form action="#" method="post" class="contact-form">
+                                <form  class="contact-form" onSubmit={this.handleSubmit}> 
                                     <div class="col-sm-6 contact-form-left">
                                         <div class="form-group">
-                                            <input name="name" type="text" class="form-control" id="name" placeholder="Name" />
-                                            <input type="email" name="email" class="form-control" id="mail" placeholder="Email" />
+                                            <input name="name" type="text" class="form-control" id="name" placeholder="Name"  onChange={this.handleInputChange} />
+                                            <input type="email" name="email" class="form-control" id="mail" placeholder="Email"   onChange={this.handleInputChange}/>
                                             <input name="subject" type="text" class="form-control" id="subject"
-                                                placeholder="Subject" />
+                                                placeholder="Subject"   onChange={this.handleInputChange} />
                                         </div>
                                     </div>
                                     <div class="col-sm-6 contact-form-right">
                                         <div class="form-group">
                                             <textarea name="message" rows="6" class="form-control" id="comment"
-                                                placeholder="Your message here..."></textarea>
-                                            <button type="submit" class="btn btn-default">Send</button>
+                                                placeholder="Your message here..."  onChange={this.handleInputChange}></textarea>
+                                          <button type="submit" class="btn btn-default" onClick={(e)=>this.handleClear} > Send</button>
+                                         
+                                          
                                         </div>
                                     </div>
                                 </form>
@@ -49,6 +107,7 @@ const ContactForm = () => {
             </Parallax>
         </div>
     );
+        }
 }
 
 
